b7d996f31ca0fb614b353c03e84b669527c98dd6
Rodrigo Arguello-Serrano <rodrigo@gmail.com>
2022-07-17 16:46:57 -0700

    Flask Assigments

--- dojo_flask/understrnading_routing/server.py
+++ dojo_flask/understrnading_routing/server.py
@@ -1,4 +1,27 @@
-import flask
-
-
 from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!!!"
+
+@app.route("/dojo")
+def dojo():
+    return "Dojo!"
+
+@app.route("/say/<name>")
+def say_name(name):
+    return f"Hi {name.title()}"
+
+@app.route("/repeat/<int:num>/<string:word>")
+def repeat(num, word):
+    output=""
+    
+    for i in range(num):
+        output+= f"<p>{word}<p/>"
+    return output
+
+if __name__ == "__main__":
+    app.run(debug=True)
